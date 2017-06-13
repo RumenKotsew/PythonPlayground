89f6fa222fda562bd487807a309644141c06da0b
RumenKotsew <kotsewrumen@gmail.com>
2017-06-13 16:34:14 +0300

    rotation challenge

---
+++ challenges/arrays_strings/rotation/rotation_challenge_test.py
@@ -0,0 +1,23 @@
+from nose.tools import assert_equal
+from rotation_challenge import Rotation
+
+
+class TestRotation(object):
+
+    def test_rotation(self):
+        rotation = Rotation()
+        assert_equal(rotation.is_rotation('o', 'oo'), False)
+        assert_equal(rotation.is_rotation(None, 'foo'), False)
+        assert_equal(rotation.is_rotation('', 'foo'), False)
+        assert_equal(rotation.is_rotation('', ''), True)
+        assert_equal(rotation.is_rotation('foobarbaz', 'barbazfoo'), True)
+        print('Success: test_rotation')
+
+
+def main():
+    test = TestRotation()
+    test.test_rotation()
+
+
+if __name__ == '__main__':
+    main()
